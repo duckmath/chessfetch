8c8ed8064376157c83654508ba40d0d99668dcbe
maddox05 <maddoxschmidlkofer@gmail.com>
2023-12-21 17:10:35 -0500

    fixed js sever but python websockets are trash

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
+WebSocket = require("ws")
+
 
 const socket = new WebSocket('ws://localhost:6969');
+
+socket.onerror = (error) => {
+    console.log("Connection Failed");
+    console.error(error);
+};
+
+socket.onopen = (event) => {
+    console.log("Connected");
+    socket.send("Hey server");
+}
+socket.onclose = (event) => {
+    console.log("Disconnected");
+}
